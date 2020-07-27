9806e6718ff8a5f2f3b45f44e9f2c86e3ad00fb8
Anders Thuné <asse.97@gmail.com>
2020-07-27 14:37:15 +0200

    Add document store to state

---
+++ src/boot/langserver/document_store.ml
@@ -0,0 +1,26 @@
+open Lsp
+module Table = Stdune.Table
+
+type t = (Uri.t, Text_document.t) Table.t
+
+let make () = Table.create (module Uri) 50
+
+let put store uri doc = Table.set store uri doc
+
+let get_opt store = Table.find store
+
+let get store uri =
+  match Table.find store uri with
+  | Some doc -> Ok doc
+  | None ->
+    Error
+      (Jsonrpc.Response.Error.make ~code:InvalidRequest
+         ~message:(Format.asprintf "no document found with uri: %a" Uri.pp uri)
+         ())
+
+let remove_document store uri =
+  match Table.find store uri with
+  | None -> ()
+  | Some _ -> Table.remove store uri
+
+let get_size store = Table.length store
--- src/boot/langserver/langserver.ml
+++ src/boot/langserver/langserver.ml
@@ -2,6 +2,17 @@ open Lsp
 open Types
 open Eval
 open Repl
+open Printf
+
+module State = struct
+  type t = {
+    store : Document_store.t
+  }
+
+  let empty () = {
+    store = Document_store.make ()
+  }
+end
 
 let make_error = Jsonrpc.Response.Error.make
 
@@ -22,22 +33,25 @@ let initialize_info : InitializeResult.t =
 
 let on_request :
     type resp.
-       unit Server.t
+       State.t Server.t
     -> resp Client_request.t
-    -> (resp * unit, Jsonrpc.Response.Error.t) result Fiber.t =
-  fun _ req ->
+    -> (resp * State.t, Jsonrpc.Response.Error.t) result Fiber.t =
+  fun server req ->
+    let state = Server.state server in
     match req with
-    | Client_request.Initialize _ -> Fiber.return @@ Ok (initialize_info, ())
-    | Client_request.Shutdown -> Fiber.return @@ Ok ((),())
+    | Client_request.Initialize _ -> Fiber.return @@ Ok (initialize_info, state)
+    | Client_request.Shutdown -> Fiber.return @@ Ok ((),state)
     | Client_request.TextDocumentCompletion
         { textDocument = { uri }; position; context = _ } ->
       let uri = Uri.t_of_yojson (`String uri) in
       let { Position.line = line; Position.character = character } = position in
       let path = Uri.to_path uri in
-      let lines = BatFile.lines_of path in
+      let lines = BatArray.of_enum (BatFile.lines_of path) in
       begin
         try
-          let prog_text = BatEnum.reduce (Printf.sprintf "%s\n%s") lines in
+          prerr_endline "Starting completion";
+          let prog_text = BatArray.reduce (sprintf "%s\n%s") lines in
+          eprintf "Program text: %s\n" prog_text; flush stderr;
           let ast = parse_prog_or_mexpr path prog_text in
           initialize_envs ();
           parsed_files := [];
@@ -45,18 +59,42 @@ let on_request :
             |> merge_includes (Filename.dirname path) [path]
             |> eval_with_envs ~skip_eval:true !repl_envs in
           repl_envs := new_envs;
-          let line_to_complete = lines
-            |> BatEnum.skip (line - 1)
-            |> BatEnum.get_exn in
+          let line_to_complete = lines.(line) in
           let _,raw_completions = get_completions line_to_complete character in
+          prerr_endline "Got completion candidates:";
+          List.iter prerr_endline raw_completions;
           let completions = List.map (fun x -> CompletionItem.create ~label:x ()) raw_completions in
-          Fiber.return @@ Ok (Some (`List completions), ())
-        with _ -> Fiber.return @@ Ok (None, ())
+          Fiber.return @@ Ok (Some (`List completions), state)
+        with e -> prerr_endline (Printexc.to_string e); Fiber.return @@ Ok (None, state)
       end
     | _ -> not_supported ()
 
-let on_notification server (_ : Client_notification.t) : unit Fiber.t =
-  Fiber.return @@ Server.state server
+let on_notification server (notification : Client_notification.t) : State.t Fiber.t =
+  let state = Server.state server in
+  let store = state.State.store in
+  match notification with
+  | TextDocumentDidOpen params ->
+    let tdoc = Text_document.make params in
+    let uri = Text_document.documentUri tdoc in
+    Document_store.put store uri tdoc;
+    Fiber.return state
+  | TextDocumentDidClose { textDocument = { uri } } ->
+    let uri = Uri.t_of_yojson (`String uri) in
+    Document_store.remove_document store uri;
+    Fiber.return state
+  | TextDocumentDidChange { textDocument = { uri; version }; contentChanges }
+    -> (
+    let uri = Uri.t_of_yojson (`String uri) in
+    match Document_store.get store uri with
+    | Error e ->
+      Format.eprintf "uri doesn't exist %s@.%!" e.message;
+      Fiber.return state
+    | Ok prev_doc ->
+      let doc = Text_document.apply_content_change ?version (List.hd contentChanges) prev_doc
+      in
+      Document_store.put store uri doc;
+      Fiber.return state )
+  | _ -> Fiber.return state
 
 let main =
   let scheduler = Scheduler.create () in
@@ -68,5 +106,5 @@ let main =
     let io = Io.make stdin stdout in
     Rpc.Stream_io.make scheduler io
   in
-  let server = Server.make handler stream () in
+  let server = Server.make handler stream (State.empty ()) in
   Scheduler.run scheduler (Server.start server)
